2ccc49bc66f84ec6b9f50c60262f67cdcb213775
Ta-Seen <taseen00.islam@gmail.com>
2024-04-30 19:50:10 +1000

    chore: format sources files

--- program_1.c
+++ program_1.c
@@ -1,59 +1,64 @@
-/******************************************************************************* 
-The assignment 3 for subject 48450 (RTOS) in University of Technology Sydney(UTS) 
-This is a template of Program_1.c template. Please complete the code based on 
-the assignment 3 requirement. Assignment 3 
+/*******************************************************************************
+The assignment 3 for subject 48450 (RTOS) in University of Technology
+Sydney(UTS) This is a template of Program_1.c template. Please complete the code
+based on the assignment 3 requirement. Assignment 3
 
 ------------------------------Program_1.c template------------------------------
 *******************************************************************************/
 
-#include <pthread.h> 	/* pthread functions and data structures for pipe */
-#include <unistd.h> 	/* for POSIX API */
-#include <stdlib.h> 	/* for exit() function */
-#include <stdio.h>	/* standard I/O routines */
-#include <stdbool.h>
-#include <string.h>
-#include <semaphore.h>
-#include <time.h>
-#include <fcntl.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <pthread.h> /* pthread functions and data structures for pipe */
+#include <semaphore.h>
+#include <stdbool.h>
+#include <stdio.h>  /* standard I/O routines */
+#include <stdlib.h> /* for exit() function */
+#include <string.h>
 #include <sys/mman.h>
-#include <sys/types.h>
 #include <sys/shm.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h> /* for POSIX API */
 
 typedef struct RR_Params {
-  //add your variables here
-  
+  // add your variables here
+
 } ThreadParams;
 
-
-
-/* this function calculates Round Robin (RR) with a time quantum of 4, writes waiting time and turn-around time to the RR */
-void *worker1(void *params)
-{
-   // add your code here
+/**
+ * This function calculates Round Robin (RR) with a time quantum of 4, writes
+ * waiting time and turn-around time to the RR.
+ */
+void *worker1(void *params) {
+  // add your code here
+  return NULL;
 }
 
-/* reads the waiting time and turn-around time through the RR and writes to text file */
-void *worker2()
-{
-   // add your code here
+/**
+ * Reads the waiting time and turn-around time through the RR and writes to text
+ * file.
+ */
+void *worker2() {
+  // add your code here
+  return NULL;
 }
 
-/* this main function creates named pipe and threads */
-int main(void)
-{
-	/* creating a named pipe(RR) with read/write permission */
-	// add your code 
+/**
+ * This main function creates named pipe and threads.
+ */
+int main(void) {
+  /* creating a named pipe(RR) with read/write permission */
+  // add your code
 
-	/* initialize the parameters */
-	 // add your code 
-	
-	/* create threads */
-	 // add your code
-	
-	/* wait for the thread to exit */
-	//add your code
-	
-	return 0;
+  /* initialize the parameters */
+  // add your code
+
+  /* create threads */
+  // add your code
+
+  /* wait for the thread to exit */
+  // add your code
+
+  return 0;
 }
