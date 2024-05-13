9eb1b45566230da66bcdb82a6363e2ec9e532ec2
Ta-Seen <taseen00.islam@gmail.com>
2024-05-13 23:34:54 +1000

    chore(program_1): provide Round Robin input parameters

--- program_1.c
+++ program_1.c
@@ -23,19 +23,36 @@ based on the assignment 3 requirement. Assignment 3
 
 /* --- Constants --- */
 
-/** The maximum string length of the output file name. */
-#define OUTPUT_FILE_NAME_LEN 100
+/** Set to 1 to turn on DEBUG, otherwise set to 0. */
+#define DEBUG 1
+
+/** Two threads, one for `worker1` and one for `worker2`. */
+#define NUM_THREADS 2
+/** Number of processes to simulate in Round Robin scheduling. */
+#define NUM_RR_PROCESSES 7
+
 /** The pipe channel to read data from. */
 #define PIPE_READ 0
 /** The pipe channel to write data to. */
 #define PIPE_WRITE 1
+/** The maximum string length of the output file name. */
+#define OUTPUT_FILE_NAME_LEN 100
 
 /* --- Global variables --- */
 
-int num_threads = 2; // two threads, one for `worker1` and one for `worker2`
 pthread_attr_t attr;
 
 /* --- Structs --- */
+
+/**
+ * Input data for each process.
+ */
+typedef struct rr_input_data_t {
+  /** When does the process arrive (in milliseconds)? */
+  int arrival_time;
+  /** CPU cycle count for this process to execute (in milliseconds). */
+  int burst_time;
+} rr_input_data_t;
 
 /**
  * Thread parameters for the Round Robin scheduler.
@@ -43,6 +60,7 @@ pthread_attr_t attr;
 typedef struct rr_params_t {
   int pipe_file[2];
   long int time_quantum;
+  rr_input_data_t rr_input_data[NUM_RR_PROCESSES];
   char output_file[OUTPUT_FILE_NAME_LEN];
 } thread_params_t;
 
@@ -60,7 +78,7 @@ void *worker1(void *params);
  */
 void *worker2(void *params);
 
-/* --- Main code -- */
+/* --- Main code --- */
 
 /**
  * This main function creates named pipe and threads.
@@ -71,8 +89,23 @@ int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
   }
 
-  pthread_t tids[num_threads]; // two threads
+  pthread_t tids[NUM_THREADS]; // two threads
   thread_params_t params;      // thread parameters
+
+  // Initialize input data for Round Robin scheduling simulation
+  rr_input_data_t rr_input_data[NUM_RR_PROCESSES] = {
+      {.arrival_time = 8, .burst_time = 10},
+      {.arrival_time = 10, .burst_time = 3},
+      {.arrival_time = 14, .burst_time = 7},
+      {.arrival_time = 9, .burst_time = 5},
+      {.arrival_time = 16, .burst_time = 4},
+      {.arrival_time = 21, .burst_time = 6},
+      {.arrival_time = 26, .burst_time = 2},
+  };
+
+  for (int i = 0; i < NUM_RR_PROCESSES; i++) {
+    params.rr_input_data[i] = rr_input_data[i];
+  }
 
   // Create a named pipe (RR) with read/write permission
   int pipe_result = pipe(params.pipe_file);
@@ -106,7 +139,7 @@ int main(int argc, char *argv[]) {
   // Wait for all threads to finish
   // Inspired by the example found in `man pthread_create`
   int thread_join_res, tnum;
-  for (tnum = 0; tnum < num_threads; tnum++) {
+  for (tnum = 0; tnum < NUM_THREADS; tnum++) {
     thread_join_res = pthread_join(tids[tnum], NULL);
     if (thread_join_res != 0) {
       errno = thread_join_res;
@@ -119,7 +152,16 @@ int main(int argc, char *argv[]) {
 }
 
 void *worker1(void *params) {
-  // add your code here
+  thread_params_t *p = params;
+
+#if DEBUG
+  printf("ID\tArrival\tBurst\n");
+  for (int i = 0; i < NUM_RR_PROCESSES; i++) {
+    printf("%d\t%d\t%d\n", i + 1, p->rr_input_data[i].arrival_time,
+           p->rr_input_data[i].burst_time);
+  }
+#endif
+
   return NULL;
 }
 
